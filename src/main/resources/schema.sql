a23ae273bb91e66cca56051104aaeaa6d25de6e4
林佳文 <linjiawen@linjiawens-MacBook-Pro.local>
2025-12-10 21:53:16 +0200

    2025-12-10 Initial data tables are created.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -18,22 +18,35 @@ CREATE TABLE IF NOT EXISTS tbl_author (
 CREATE TABLE IF NOT EXISTS tbl_user (
     id VARCHAR(36) PRIMARY KEY,
     name VARCHAR(36) NOT NULL,
+    email VARCHAR(36) NOT NULL,
+    phone VARCHAR(36) NOT NULL,
+    password VARCHAR(12) NOT NULL,
     nationality VARCHAR(12) NOT NULL,
-    birthday VARCHAR(36) NOT NULL
+    birthday VARCHAR(36) NOT NULL,
     create_at VARCHAR(36) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS tbl_booklist (
-    id varchar(36) PRIMARY KEY,
-    user_id varchar(36) NOT NULL,
-    category varchar(12) NOT NULL,
+CREATE TABLE IF NOT EXISTS tbl_rel_userbook (
+    user_id VARCHAR(36) NOT NULL,
+    book_id VARCHAR(36) NOT NULL,
+    status VARCHAR(12) NOT NULL,
+    rating integer NOT NULL,
     created_at VARCHAR(36) NOT NULL,
-    description VARCHAR(200)
-)
+    PRIMARY KEY (user_id, book_id)
+);
 
-CREATE TABLE IF NOT EXISTS tbl_booklistdetail (
-    list_id VARCHAR(36) PRIMARY KEY,
-    book_id VARCHAR(36) PRIMARY KEY,
-    book_name VARCHAR(36)
-)
+CREATE TABLE IF NOT EXISTS tbl_bookexcerpt (
+    id VARCHAR(36) NOT NULL,
+    book_id VARCHAR(36) NOT NULL,
+    excerpt VARCHAR(2048) NOT NULL,
+    comment VARCHAR(200) NOT NULL,
+    created_at VARCHAR(36) NOT NULL,
+    PRIMARY KEY (id, book_id)
+);
 
+CREATE TABLE IF NOT EXISTS tbl_manager (
+    id VARCHAR(36) PRIMARY KEY,
+    name VARCHAR(36) NOT NULL,
+    password VARCHAR(12) NOT NULL,
+    created_at VARCHAR(36) NOT NULL
+);
